fdc974165ccba627e760842cc425b22bf6814a23
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2023-09-05 21:37:11 -0600

    Add TopN functionality to processes

--- pkg/routes/process.go
+++ pkg/routes/process.go
@@ -5,10 +5,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/shirou/gopsutil/v3/process"
 	"net/http"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func ProcessHandler(c *gin.Context) {
+	topNProcessesString := c.DefaultQuery("top", "all")
+	var topNProcesses = -1
+	if strings.ToLower(topNProcessesString) != "all" {
+		i, err := strconv.ParseInt(topNProcessesString, 10, 64)
+		if err != nil {
+			_ = c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		topNProcesses = int(i)
+	}
+
 	rtnMe := strings.Builder{}
 
 	processes, err := process.Processes()
@@ -27,7 +40,20 @@ func ProcessHandler(c *gin.Context) {
 `
 	rtnMe.WriteString(openMetricsMetadata)
 
-	for _, p := range processes {
+	// cpu
+	sort.Slice(processes, func(i, j int) bool {
+		iPercent, err2 := processes[i].CPUPercentWithContext(c.Request.Context())
+		if err2 != nil {
+			panic(err2)
+		}
+		jPercent, err2 := processes[j].CPUPercentWithContext(c.Request.Context())
+		if err2 != nil {
+			panic(err2)
+		}
+		return iPercent < jPercent
+	})
+
+	for i, p := range processes {
 		cmd, _ := p.CmdlineWithContext(c.Request.Context())
 		if cmd == "" {
 			cmd, _ = p.ExeWithContext(c.Request.Context())
@@ -36,13 +62,45 @@ func ProcessHandler(c *gin.Context) {
 		ppid, _ := p.PpidWithContext(c.Request.Context())
 		name, _ := p.NameWithContext(c.Request.Context())
 		cpuPercent, _ := p.CPUPercentWithContext(c.Request.Context())
+
+		rtnMe.WriteString(fmt.Sprintf(`process_cpu_percent{name="%s",cmdLine="%s",pid="%d",ppid="%d"} %f`+"\n", name, cmd, pid, ppid, cpuPercent/100))
+
+		if i == topNProcesses-1 {
+			break
+		}
+	}
+	// memory
+	sort.Slice(processes, func(i, j int) bool {
+		iMem, err2 := processes[i].MemoryInfoWithContext(c.Request.Context())
+		if err2 != nil {
+			panic(err2)
+		}
+		iMemVal := iMem.RSS
+		jMem, err2 := processes[j].MemoryInfoWithContext(c.Request.Context())
+		if err2 != nil {
+			panic(err2)
+		}
+		jMemVal := jMem.RSS
+		return iMemVal < jMemVal
+	})
+
+	for i, p := range processes {
+		cmd, _ := p.CmdlineWithContext(c.Request.Context())
+		if cmd == "" {
+			cmd, _ = p.ExeWithContext(c.Request.Context())
+		}
+		pid := p.Pid
+		ppid, _ := p.PpidWithContext(c.Request.Context())
+		name, _ := p.NameWithContext(c.Request.Context())
 		mem, _ := p.MemoryInfoWithContext(c.Request.Context())
 		memRss := mem.RSS
 		//mem, _ := p.MemoryInfoWithContext(c.Request.Context())
 
-		rtnMe.WriteString(fmt.Sprintf(`process_cpu_percent{name="%s",cmdLine="%s",pid="%d",ppid="%d"} %f`+"\n", name, cmd, pid, ppid, cpuPercent/100))
 		rtnMe.WriteString(fmt.Sprintf(`process_memory_rss_bytes{name="%s",cmdLine="%s", pid="%d",ppid="%d"} %d`+"\n", name, cmd, pid, ppid, memRss))
 
+		if i == topNProcesses-1 {
+			break
+		}
 	}
 
 	c.Data(http.StatusOK, "application/openmetrics-text; version=1.0.0; charset=utf-8", []byte(rtnMe.String()))
--- tests/v1/process_test.go
+++ tests/v1/process_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"random-exporters/pkg/middleware"
+	"strings"
 	"testing"
 	"time"
 )
@@ -41,6 +42,51 @@ func Test(t *testing.T) {
 			}
 			//t.Log(w.Body.String())
 			t.Logf("Time taken: %d ms", endTime.Sub(startTime).Milliseconds())
+		})
+	}
+}
+
+func TestTopNProcesses(t *testing.T) {
+	tests := []struct {
+		name               string
+		path               string
+		method             string
+		expectedCode       int
+		expectedNumResults int
+	}{
+		{
+			name:               "List processes",
+			path:               "/v1/prom/processes?top=20",
+			method:             "GET",
+			expectedCode:       http.StatusOK,
+			expectedNumResults: 20,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gin.SetMode(gin.ReleaseMode)
+			e := gin.New()
+			router := middleware.GenerateRouter(e)
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest(test.method, test.path, nil)
+			startTime := time.Now()
+			router.ServeHTTP(w, req)
+			endTime := time.Now()
+			code := w.Code
+
+			if test.expectedCode != code {
+				t.Errorf("Expected code %d got %d", test.expectedCode, code)
+			}
+			//t.Log(w.Body.String())
+			t.Logf("Time taken: %d ms", endTime.Sub(startTime).Milliseconds())
+
+			body := w.Body.String()
+			totalLines := strings.Count(body, "\n")
+
+			if totalLines-7 != test.expectedNumResults*2 {
+				t.Errorf("Expected %d results got %d", test.expectedNumResults*2, totalLines-7)
+			}
 		})
 	}
 }
